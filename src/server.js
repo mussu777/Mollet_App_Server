858e88019d1aa0310f70771b761ae0338abe89e0
mussu777 <mustak65ee@gmail.com>
2025-07-15 23:38:55 +0530

    changes in server

--- src/server.js
+++ src/server.js
@@ -15,7 +15,7 @@ app.use(rateLimiter);
 app.use(express.json());
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.get('/api/health', (req, res) => {
   res.status(200).json({ status: "ok" });
